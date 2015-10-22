ecbe50781372c8b4a47cbe484daff933edd121d1
Justin Herrera <herreraju@mullenmail.com>
2015-10-22 13:43:18 -0600

    JustCHang

--- rubik.js
+++ rubik.js
@@ -1,43 +1,77 @@
-cube.style.webkitTransform = 
-"rotateX(" + x + "deg) rotateY(" + y + "deg)";
+//cube.style.webkitTransform =
+//"rotateX(" + x + "deg) rotateY(" + y + "deg)";
 
 //test
+//uf
+function Edge(st1, st2, st3)
+    {
+    this.st1 = st1;
+    this.st2 = st2;
+    this.st3 = st3;
+    };
+var edg = new Edge("k", "", "");
 
-var Side = function(c1, c2, c3, c4, c5, c6, c7, c8, c9){
-    this.appendEdge(co1, co2, co3, p1, p2, p3)
+function Side(c1, c2, c3, c4, c5, c6, c7, c8, c9){
+    //this.appendEdge = new function(edg , p1, p2, p3)
+    //    {
+    //    this[("c" + p1)] = edg.st1;
+    //    this[("c" + p2)] = edg.st2;
+    //    this[("c" + p3)] = edg.st3;
+    //    };
+    this.getEdge = function(num1, num2, num3)
         {
-        this[("c" + p1.toString())] = co1;
-        this[("c" + p2.toString())] = co2;
-        this[("c" + p3.toString())] = co3;
-        };
-    this.getEdge(num1, num2, num3)
-        {
-        var a = "s" + num1.toString();
-        var b = "s" + num2.toString();
-        var c = "s" + num3.toString();
-        return {this[a], this[b], this[c]};
-        };
-    this.setAll(clr)
-        {
-        for(var i = 1; i <= 9; i++)
+        var i = 1;
+        var a;
+        var b;
+        var c;
+        for(var key in this)
             {
-            this[("c" + i.toString)] = clr;
+            if(num1 == i)
+                {
+                a = key;
+                }
+            if(num2 == i)
+                {
+                b = key;
+                }
+            if(num1 == i)
+                {
+                c = key;
+                }
             }
+        var e = new Edge(this[a], this[b], this[c]);
+        return e;
         };
-    this.rotate(numOfRot)
+    this.rotate = function(numOfRot)
         {
-        for(i=0; i<numOfRot; i++)
+        for(var i=0; i<numOfRot; i++)
             {
             var temp1 = c1;
             var temp2 = c2;
             var temp3 = c3;
-            c1= c7;
+            c1 = c7;
             }
         };
-    this.getAll()
+
+this.setAll = function(clr)
+    {
+    console.log(clr);
+        for(var key in this)
         {
-        return {this.c1, this.c2, this.c3, this.c4, this.c5, this.c6, this.c7, this.c8, this.c9};
-        };
+            this[key] = clr;
+        }
+    };
+    this.c1 = c1;
+    this.c2 = c2;
+    this.c3 = c3;
+    this.c4 = c4;
+    this.c5 = c5;
+    this.c6 = c6;
+    this.c7 = c7;
+    this.c8 = c8;
+    this.c9 = c9;
 };
-var side1 = new Side();
+
+var side1 = new Side("b", "a", "b", "a", "b", "a", "b", "a", "b");
 side1.setAll("blue");
+console.log(side1.c7);
